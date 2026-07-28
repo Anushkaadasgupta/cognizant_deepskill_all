51dd768004865ef59649ef6b7a849c28c5a519ff
Anushkaadasgupta <dasguptaanushka2005@gmail.com>
2026-07-28 20:29:56 +0530

    Add Spring REST Hands-on 3 Employee REST Service

---
+++ week 2/3. Spring REST Hands-on/employee-rest-service/src/main/java/com/cognizant/employeerestservice/dao/EmployeeDao.java
@@ -0,0 +1,30 @@
+package com.cognizant.employeerestservice.dao;
+
+import java.util.ArrayList;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Repository;
+
+import com.cognizant.employeerestservice.model.Employee;
+
+@Repository
+public class EmployeeDao {
+
+    private static ArrayList<Employee> EMPLOYEE_LIST;
+
+    @SuppressWarnings("unchecked")
+    public EmployeeDao() {
+
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("employee.xml");
+
+        EMPLOYEE_LIST =
+                (ArrayList<Employee>) context.getBean("employeeList");
+
+        context.close();
+    }
+
+    public ArrayList<Employee> getAllEmployees() {
+        return EMPLOYEE_LIST;
+    }
+}
---
+++ week 2/3. Spring REST Hands-on/employee-rest-service/src/main/java/com/cognizant/employeerestservice/service/EmployeeService.java
@@ -0,0 +1,22 @@
+package com.cognizant.employeerestservice.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cognizant.employeerestservice.dao.EmployeeDao;
+import com.cognizant.employeerestservice.model.Employee;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    @Transactional
+    public ArrayList<Employee> getAllEmployees() {
+        return employeeDao.getAllEmployees();
+    }
+}
